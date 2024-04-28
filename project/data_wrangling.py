7be5f0b8fba51e39441eceea9af36221707c5496
Daishin Murooka <dmurooka@gmail.com>
2024-04-28 14:48:46 +0900

    Initiate data wrangling project

---
+++ project/data_wrangling.py
@@ -0,0 +1,9 @@
+import numpy as nm
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv(
+    "/Users/daishinmurooka/Desktop/data-wrangling/project/js244v0000_1.csv"
+)
+
+df.info()
